6e07ca145e9318a845e1e855b3be4b0aef3bd020
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-02-03 21:18:51 +0600

    add

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="user_home")
+    path('', views.home, name="user_home"),
+    path('user_store/', views.store, name="user_store"),
+    path('user_cart/', views.cart, name="user_cart"),
+    path('user_checkout/', views.checkout, name="user_checkout"),
 ]
--- user_panel/views.py
+++ user_panel/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-     return render(request, 'user_panel/home.html')    
+     return render(request, 'user_panel/home.html')  
+def store(request):
+     return render(request, 'user_panel/store.html')     
+def cart(request):
+     return render(request, 'user_panel/cart.html') 
+def checkout(request):
+     return render(request, 'user_panel/checkout.html')                  
