91f72a0c033cab19add6497cd956f8ac1b1de0fb
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-02-10 00:29:14 +0600

    add cart.

--- user_panel/views.py
+++ user_panel/views.py
@@ -4,16 +4,36 @@ from .models import *
 
 # Create your views here.
 
+
 def home(request):
-     return render(request, 'user_panel/home.html')  
+    return render(request, 'user_panel/home.html')
+
+
 def store(request):
-     products = Product.objects.all()
-     context = {}
-     context = {
-          'products': products,
-     }
-     return render(request, 'user_panel/store.html', context)     
+    products = Product.objects.all()
+    context = {}
+    context = {
+        'products': products,
+    }
+    return render(request, 'user_panel/store.html', context)
+
+
 def cart(request):
-     return render(request, 'user_panel/cart.html') 
+    ord = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        ord = order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {
+        'items': items,
+        'order': ord,
+    }
+    return render(request, 'user_panel/cart.html', context)
+
+
 def checkout(request):
-     return render(request, 'user_panel/checkout.html')                  
+    return render(request, 'user_panel/checkout.html')
