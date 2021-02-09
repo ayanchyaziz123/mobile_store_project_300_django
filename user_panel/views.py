abc4920f33db92d146be079def17e82ae1ea731c
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-02-09 20:02:45 +0600

    complete dynamic image

--- user_panel/views.py
+++ user_panel/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
      return render(request, 'user_panel/home.html')  
 def store(request):
-     return render(request, 'user_panel/store.html')     
+     products = Product.objects.all()
+     context = {}
+     context = {
+          'products': products,
+     }
+     return render(request, 'user_panel/store.html', context)     
 def cart(request):
      return render(request, 'user_panel/cart.html') 
 def checkout(request):
