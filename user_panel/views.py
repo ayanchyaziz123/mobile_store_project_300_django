e4d8ff64d529cc56eff7f252cc19dad829d0ba14
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-02-10 21:45:01 +0600

    complete ds part

--- user_panel/views.py
+++ user_panel/views.py
@@ -19,14 +19,13 @@ def store(request):
 
 
 def cart(request):
-    ord = []
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         items = []
-        order = []
+        order = {'get_cart_total':0, 'get_cart_items': 0}
 
     context = {
         'items': items,
@@ -37,4 +36,17 @@ def cart(request):
 
 
 def checkout(request):
-    return render(request, 'user_panel/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+
+    }
+    return render(request, 'user_panel/checkout.html', context)
