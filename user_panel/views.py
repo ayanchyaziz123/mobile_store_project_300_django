7ea85c98a2082254212c65f4ee0a255be8395c86
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-02-12 15:28:24 +0600

    add make paymet button

--- user_panel/views.py
+++ user_panel/views.py
@@ -19,7 +19,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items': 0}
+        order = {'get_cart_total':0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
@@ -41,7 +41,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items': 0}
+        order = {'get_cart_total':0, 'get_cart_items': 0,  'shipping': False}
         cartItems = order['get_cart_items']
 
     context = {
@@ -61,7 +61,7 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items': 0}
+        order = {'get_cart_total':0, 'get_cart_items': 0,  'shipping': False}
         cartItems = order['get_cart_items']
 
     context = {
