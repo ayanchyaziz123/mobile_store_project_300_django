f0de2a4b11f340462b6df34645d43469592b5d0a
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-03-16 13:50:03 +0600

    added login

--- mobile_store/user_auth/views.py
+++ mobile_store/user_auth/views.py
@@ -2,11 +2,30 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def logIn(request):
-    return render(request, 'user_panel/login.html')
+   
+    if request.method == "POST":
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Successfully logged in')
+            return redirect('user_home')
+        else:
+            messages.error(request, 'Not logged in')
+            return render(request, 'user_panel/login.html')
+
+    else:  
+        return render(request, 'user_panel/login.html')  
+
+
+
+    
 
 def logOut(request):
     logout(request)
